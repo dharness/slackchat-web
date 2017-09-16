a7e10567a721cbbaa79a10f40ad8e5ac6ac49e79
Dylan Harness <dharness@uwo.ca>
2017-09-16 15:07:20 -0400

    made login styles

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,7 +2,13 @@ import axios from 'axios'
 const API_URL = 'http://localhost:9090/api'
 
 export const login = async ({ email, password }) => {
-  const response = await axios.post(`${API_URL}/auth/login`, { email, password })
-  const { token, id } = response.data;
-  return { token, id };
+  try {
+    const response = await axios.post(`${API_URL}/auth/login`, { email, password })
+    const { token, id } = response.data;
+    return { token, id };
+  } catch ({ response }) {
+    return {
+      error: { text: 'No such account.' }
+    }
+  }
 }
--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -14,36 +14,68 @@ class Login extends Component {
     super();
     this.state = {
       email: '',
-      password: ''
-    }
+      password: '',
+      errorMessage: ''
+    };
+    this.emailTemplate = `
+      Hey pal,
+
+      I forgot my password for slackchat. I know, I know, I should have written it down.
+      But can you reset it for me?
+      My email address is YOUR_EMAIL_ADDRESS
+
+      Sincerely,
+      A forgetful user
+    `;
   }
 
   async login() {
-    const { token, id, email } = await login(this.state);
-    this.props.account.authToken = token;
-    this.props.account.id = id;
-    this.props.history.push(`${id}/dashboard/analytics`)
+    const { token, id, email, error } = await login(this.state);
+    if(error) {
+      this.setState({ errorMessage: error.text })
+    } else {
+      this.props.account.authToken = token;
+      this.props.account.id = id;
+      this.props.history.push(`${id}/dashboard/analytics`)
+    }
   }
 
   @autobind
   handleEmailChange(event) {
-    this.setState({ email: event.target.value });
+    this.setState({ email: event.target.value, errorMessage: '' });
   }
 
   @autobind
   handlePasswordChange(event) {
-    this.setState({ password: event.target.value });
+    this.setState({ password: event.target.value, errorMessage: '' });
   }
 
   render () {
+    const canSubmit = this.state.email.length > 0 &&
+        this.state.password.length > 0 &&
+        this.state.errorMessage.length === 0;
     return (
       <div className="sc-auth">
         <img src={logoUrl} className="sc-auth--logo" />
         <div className="sc-auth--logo-text">slackchat</div>
+        {this.state.errorMessage.length > 0 && 
+          <div className="sc-auth--error-message">
+            {this.state.errorMessage}
+          </div>
+        }
+
         <input className="sc-auth--input" type="email" onChange={this.handleEmailChange}/>
         <input className="sc-auth--input" type="password" onChange={this.handlePasswordChange}/>
-        <Link to="auth" className="sc-auth--password-link">Forgot your password?</Link>
-        <button className="sc-auth--button" onClick={this.login.bind(this)}>
+        <Link
+          to={`mailto:hello@kingofthestack.com?subject=Forgot Password&body=${encodeURI(this.emailTemplate)}`}
+          className="sc-auth--password-link">
+            Forgot your password?
+        </Link>
+        <button
+          className={"sc-auth--button" + (canSubmit ? "" : " disabled")}
+          onClick={this.login.bind(this)}
+          disabled={!canSubmit}
+        >
           Login
         </button>
         <Link to="auth" className="sc-auth--account-link">
